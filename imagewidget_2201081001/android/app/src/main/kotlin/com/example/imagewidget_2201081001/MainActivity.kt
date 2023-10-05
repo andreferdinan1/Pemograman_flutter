b1acd31814991c1cc47b4feeb328c41b97175fe9
andreferdinan1 <andreferdinand276@gmail.com>
2023-10-05 22:51:50 +0700

    update

---
+++ imagewidget_2201081001/android/app/src/main/kotlin/com/example/imagewidget_2201081001/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.imagewidget_2201081001
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ imagewidget_2201081001/lib/main.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text("Image Widget"),
+        ),
+        body: Center(
+          child: Container(
+            width: 350,
+            height: 500,
+            color: Colors.amber,
+              child: Image.network(
+                "https://picsum.photos/200",
+            // child: Image(
+            //   fit: BoxFit.cover,
+            //   // image: AssetImage("images/image.jpg")
+            //   image: NetworkImage("https://picsum.photos/id/237/200/300"),
+              ),
+          ),
+        ),
+      ),
+    );
+    
+  }
+}
