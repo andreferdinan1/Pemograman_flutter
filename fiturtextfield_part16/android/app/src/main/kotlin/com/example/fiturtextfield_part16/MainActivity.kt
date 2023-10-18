71655975f43ec208ec5d8f19df1d26354748e510
andreferdinan1 <andreferdinand276@gmail.com>
2023-10-18 21:52:36 +0700

    update

---
+++ fiturtextfield_part16/android/app/src/main/kotlin/com/example/fiturtextfield_part16/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.fiturtextfield_part16
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
--- fiturtextfield_part16/test/widget_test.dart
+++ fiturtextfield_part16/test/widget_test.dart
@@ -8,7 +8,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:tabbar_widget_part15/main.dart';
+import 'package:fiturtextfield_part16/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
