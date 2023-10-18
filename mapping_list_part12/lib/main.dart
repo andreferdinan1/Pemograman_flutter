f9924b81296d14902b8c165f60fbbc22b07ef8d2
andreferdinan1 <andreferdinand276@gmail.com>
2023-10-18 20:36:03 +0700

    update

--- mapping_list_part12/lib/main.dart
+++ mapping_list_part12/lib/main.dart
@@ -5,8 +5,182 @@ void main() {
 }
 
 class MyApp extends StatelessWidget {
+  final List<Map<String,dynamic>> myList = [
+    {
+      "Name" : "Andre",
+      "Age" : 20,
+      "favColor": [
+      "Black",
+      "Red",
+      "Amber",
+      "Black",
+      "Red",
+      "Amber",
+      "Black",
+      "Red",
+      "Amber",
+      "Black",
+      "Red",
+      "Amber",
+      "Black",
+      "Red",
+      "Amber",
+      ],
+    },
+    {
+      "Name" : "Akbar",
+      "Age" : 21,
+      "favColor": [
+        "White",
+        "Red",
+        "Green",
+         "White",
+        "Red",
+        "Green",
+         "White",
+        "Red",
+        "Green",
+         "White",
+        "Red",
+        "Green",
+         "White",
+        "Red",
+        "Green",
+         "White",
+        "Red",
+        "Green",
+        ],
+    },
+    {
+      "Name" : "Andre",
+      "Age" : 20,
+      "favColor": [
+      "Black",
+      "Red",
+      "Amber",
+      "Black",
+      "Red",
+      "Amber",
+      "Black",
+      "Red",
+      "Amber",
+      "Black",
+      "Red",
+      "Amber",
+      "Black",
+      "Red",
+      "Amber",
+      ],
+    },
+    {
+      "Name" : "Akbar",
+      "Age" : 21,
+      "favColor": [
+        "White",
+        "Red",
+        "Green",
+         "White",
+        "Red",
+        "Green",
+         "White",
+        "Red",
+        "Green",
+         "White",
+        "Red",
+        "Green",
+         "White",
+        "Red",
+        "Green",
+         "White",
+        "Red",
+        "Green",
+        ],
+    },
+    {
+      "Name" : "Andre",
+      "Age" : 20,
+      "favColor": [
+      "Black",
+      "Red",
+      "Amber",
+      "Black",
+      "Red",
+      "Amber",
+      "Black",
+      "Red",
+      "Amber",
+      "Black",
+      "Red",
+      "Amber",
+      "Black",
+      "Red",
+      "Amber",
+      ],
+    },
+  ];
   @override
   Widget build(BuildContext context) {
-    return MaterialApp();
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text("ID Apps"),
+        ),
+        body: ListView(
+          children: myList.map((data){
+            print(data["favColor"]);
+            List myfavcolor = data["favColor"];
+            print("=======");
+            print(myfavcolor);
+            return Card(
+              margin: EdgeInsets.all(20),
+              color: Colors.black.withOpacity(0.1),
+              child: Container(
+                margin: EdgeInsets.all(10),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+
+                    Row(
+                      children: [
+                        CircleAvatar(),
+                        SizedBox(
+                          width: 20,
+
+                        ),
+                        Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Text("Name: ${data['Name']}"),
+                            Text("Age: ${data['Age']}"),
+                          ],
+                        ),
+                      ],
+                    ),
+                    //favColor
+                    SingleChildScrollView(
+                      scrollDirection: Axis.horizontal,
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.start,
+                        children: myfavcolor.map((color){
+                          return Container(
+                            color: Colors.amber,
+                            margin: EdgeInsets.symmetric(
+                              vertical: 15,
+                              horizontal: 8,
+                            ),
+                            padding: EdgeInsets.all(10),
+                            child: Text(color),
+                          );
+                        }).toList(),
+                      ),
+                    )
+                  ],
+                ),
+              ),
+            );
+          }).toList(),
+        ),
+        ),
+    );
   }
 }
