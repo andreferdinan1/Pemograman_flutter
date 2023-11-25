67f0858b77d67b1c32d51942169bfbccd9553423
andreferdinan1 <andreferdinand276@gmail.com>
2023-11-25 09:38:45 +0700

    update

---
+++ mahasiswa/src/main/java/com/andre/mahasiswa/repository/MahasiswaRepository.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
+ */
+package com.andre.mahasiswa.repository;
+import com.andre.mahasiswa.entity.Mahasiswa;
+import java.util.Optional;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+/**
+ *
+ * @author andreferdinan
+ */
+@Repository
+public interface MahasiswaRepository extends JpaRepository<Mahasiswa, Long> {
+    
+    public Optional<Mahasiswa> findMahasiswaByEmail(String email);
+    
+}
